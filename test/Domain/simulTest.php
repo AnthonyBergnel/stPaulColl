b6f484fb913d49935579262a43b75da53b6a2b9b
15bergnela <15bergnela@D22-P16-W7.estransup.local>
2017-04-06 11:42:15 +0200

    fourth

---
+++ test/Domain/simulTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace stpaul\Domain;
+require_once __DIR__.'/../../src/stpaul/IHM/Simul.php';
+
+/**
+ * Class SejourTest
+ * @package stpaul\Domain
+ */
+class SimulTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var sejour
+     */
+    protected $object;
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new sejour(1, 'Classe de mer', 170, '2016-05-02', 10);
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    public function testDteFin()
+    {
+        $this->setUp(1, 'Classe de mer', 170, '2016-05-02', 10);
+
+        $resultatAttendu = '2016-05-12';
+        $resultatObserve = $this->object->getSejDteFin();
+
+        $this->assertEquals($resultatAttendu, $resultatObserve);
+
+    }
+}
